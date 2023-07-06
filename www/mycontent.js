949d5d23e5244594c91e49d4993209285ecd8d25
DennyKuo0809 <ktying8989@gmail.com>
2023-07-06 08:38:25 +0800

    Add cpu and vehicle status

--- www/mycontent.js
+++ www/mycontent.js
@@ -1,3 +1,32 @@
+function status_on_click(btn) {
+    const scope = btn.innerHTML;
+    const api_server = "http://127.0.0.1:8000/";
+    $.ajax({
+        type: "GET",
+        url: api_server + 'status/' + scope,
+        success: function(data) {
+            // document.getElementById("status-text").innerHTML = data.all.status;
+            var velocity = (data.vehicle.status.twist.linear.x) * (data.vehicle.status.twist.linear.x) + (data.vehicle.status.twist.linear.y) * (data.vehicle.status.twist.linear.y);
+            var info = `CPU overview  (${scope})`;
+            info += `\n    * Idle:        ${data.cpu.all.idle}%`;
+            info += `\n    * In use:    ${data.cpu.all.total}%`;
+            info += `\n    * System:  ${data.cpu.all.sys}%`;
+            info += `\n    * User:       ${data.cpu.all.usr}%`;
+            info += `\nVehicle status  (${scope})`;
+            info += `\n    * Trun:     ${data.vehicle.status.turn_signal.data}`;
+            info += `\n    * Gear:     ${data.vehicle.status.gear_shift.data}`;
+            info += `\n    * Steering:    ${data.vehicle.status.steering.data}`;
+            info += `\n    * Velocity:    ${velocity}`;
+
+            alert(info);
+            console.log(data);
+        },
+        error: function(error) {
+            console.log(error);
+        },
+    });
+}
+
 $(document).ready(function() {
     const api_server = "http://127.0.0.1:8000/"
     $.ajax({
@@ -20,7 +49,7 @@ $(document).ready(function() {
             success: function(data) {
                 $('#autoware-scope-list').empty();
                 for(var i = 0 ; i < data.length ; i ++){
-                    $('#autoware-scope-list').append(`<li><a href="">${data[i].scope}</a><ul><li>address: ${data[i].address}</li></ul></li>`);
+                    $('#autoware-scope-list').append(`<li><button id='scope-link-${data[i].scope}' onclick="status_on_click(this)">${data[i].scope}</button><ul><li>Address: ${data[i].address}</li></ul></li>`);
                 }
                 console.log(data);
             },
@@ -30,3 +59,5 @@ $(document).ready(function() {
         });
     });
 });
+
+
