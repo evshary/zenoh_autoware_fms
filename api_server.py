6dd27f3ec1f95a17e1717636e9c7af685a3b2a27
DennyKuo <45343949+DennyKuo0809@users.noreply.github.com>
2023-07-19 11:02:25 +0800

    Add: teleop on autoware (#4)
    * Add: teleop on autoware
    
    * Add steering wheel
    
    * Set maximum steering angle

--- api_server.py
+++ api_server.py
@@ -1,8 +1,11 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from typing_extensions import Annotated
 from zenoh_app.list_autoware import list_autoware
 from zenoh_app.status_autoware import *
+from zenoh_app.teleop_autoware import *
 import zenoh
+import math
 
 app = FastAPI()
 app.add_middleware(
@@ -12,6 +15,7 @@ app.add_middleware(
 
 conf = zenoh.Config.from_file('config.json5')
 session = zenoh.open(conf)
+manual_controller = None
 
 @app.get("/")
 async def root():
@@ -27,3 +31,51 @@ async def manage_status_autoware(scope):
         "cpu": get_cpu_status(session, scope),
         "vehicle": get_vehicle_status(session, scope)
     }
+
+@app.get("/teleop/startup")
+async def manage_teleop_startup(scope):
+    global manual_controller
+    if manual_controller is not None:
+        manual_controller.stop_teleop()
+    manual_controller = ManualController(session, scope)
+    return f"Startup manual control on {scope}."
+
+@app.get("/teleop/gear")
+async def manage_teleop_gear(scope, gear):
+    global manual_controller
+    if manual_controller is not None:
+        manual_controller.pub_gear(gear)
+        return f"Set gear {gear} to {scope}."
+    else:
+        return "Please startup the teleop first"
+
+@app.get("/teleop/velocity")
+async def manage_teleop_speed(scope, velocity):
+    global manual_controller
+    if manual_controller is not None:
+        manual_controller.update_control_command(float(velocity) * 1000 / 3600, None)
+        return f"Set speed {velocity} to {scope}."
+    else:
+        return "Please startup the teleop first"
+
+@app.get("/teleop/turn")
+async def manage_teleop_turn(scope, angle):
+    global manual_controller
+    if manual_controller is not None:
+        manual_controller.update_control_command(None, float(angle) * math.pi / 180)
+        return f"Set steering angle {angle}."
+    else:
+        return "Please startup the teleop first"
+
+
+@app.get("/teleop/status")
+async def manage_teleop_status():
+    global manual_controller
+    if manual_controller is not None:
+        return {
+            'velocity': manual_controller.current_velocity,
+            'gear': manual_controller.current_gear,
+            'steering': manual_controller.current_steer * 180 / math.pi
+        }
+    else:
+        return "Please startup the teleop first"
--- zenoh_app/structure.py
+++ zenoh_app/structure.py
@@ -1,6 +1,6 @@
 from dataclasses import dataclass
 from pycdr2 import IdlStruct,Enum
-from pycdr2.types import int8, uint8, int32, uint32, float32, float64, sequence
+from pycdr2.types import int8, uint8, int32, int64, uint32, uint64, float32, float64, sequence
 
 @dataclass
 class Time(IdlStruct, typename="Time"):
@@ -44,7 +44,7 @@ class Steering(IdlStruct, typename="Steering"):
 
 @dataclass
 class TurnSignal(IdlStruct, typename="TurnSignal"):
-    class SIGNAL(Enum):
+    class TURN(Enum):
         NONE = 0
         LEFT = 1
         RIGHT = 2
@@ -73,3 +73,61 @@ class VehicleStatus(IdlStruct, typename="VehicleStatus"):
 class vehicle_status(IdlStruct, typename="vehicle_status"):
     stamp: Time
     status: VehicleStatus
+
+@dataclass
+class ControlCommand(IdlStruct, typename="ControlCommand"):
+    steering_angle: float64
+    steering_angle_velocity: float64
+    throttle: float64
+    brake: float64
+
+@dataclass
+class ControlCommandStamped(IdlStruct, typename="ControlCommandStamped"):
+    stamp: Time
+    control: ControlCommand
+
+@dataclass
+class GearShiftStamped(IdlStruct, typename="GearShiftStamped"):
+    stamp: Time
+    gear_shift: GearShift
+
+@dataclass
+class TurnSignalStamped(IdlStruct, typename="GearShiftStamped"):
+    stamp: Time
+    turn_signal: TurnSignal
+    
+@dataclass
+class GateMode(IdlStruct, typename="GateMode"):
+    class MODE(Enum):
+        AUTO = 0
+        EXTERNAL = 1
+    data: uint8
+
+@dataclass
+class AckermannLateralCommand(IdlStruct, typename="AckermannLateralCommand"):
+    stamp: Time
+    steering_tire_angle: float32
+    steering_tire_rotation_rate: float32
+
+@dataclass
+class LongitudinalCommand(IdlStruct, typename="LongitudinalCommand"):
+   stamp: Time
+   speed: float32
+   acceleration: float32
+   jerk: float32
+
+@dataclass
+class AckermannControlCommand(IdlStruct, typename="AckermannControlCommand"):
+    stamp: Time
+    lateral: AckermannLateralCommand
+    longitudinal: LongitudinalCommand
+
+@dataclass
+class ServiceHeader(IdlStruct, typename="ServiceHeader"):
+    guid: int64
+    seq: uint64
+
+@dataclass
+class EngageReq(IdlStruct, typename="EngageReq"):
+    header: ServiceHeader
+    engage: bool
