a16a6aad5683902c39812161bef1dee09176a7f8
ChenYing Kuo <evshary@gmail.com>
2023-09-04 15:30:21 +0800

    Remove login.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,15 +2,15 @@ import React, { lazy, useEffect } from 'react'
 import './App.css';
 import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom'
 import { themeChange } from 'theme-change'
-import checkAuth from './app/auth';
+//import checkAuth from './app/auth';
 import initializeApp from './app/init';
 
 // Importing pages
 const Layout = lazy(() => import('./containers/Layout'))
-const Login = lazy(() => import('./pages/Login'))
-const ForgotPassword = lazy(() => import('./pages/ForgotPassword'))
-const Register = lazy(() => import('./pages/Register'))
-const Documentation = lazy(() => import('./pages/Documentation'))
+//const Login = lazy(() => import('./pages/Login'))
+//const ForgotPassword = lazy(() => import('./pages/ForgotPassword'))
+//const Register = lazy(() => import('./pages/Register'))
+//const Documentation = lazy(() => import('./pages/Documentation'))
 
 
 // Initializing different libraries
@@ -18,7 +18,8 @@ initializeApp()
 
 
 // Check for login and initialize axios
-const token = checkAuth()
+// Disable login
+//const token = checkAuth()
 
 
 function App() {
@@ -33,15 +34,22 @@ function App() {
     <>
       <Router>
         <Routes>
+          {/*
+          // Disable login
           <Route path="/login" element={<Login />} />
           <Route path="/forgot-password" element={<ForgotPassword />} />
           <Route path="/register" element={<Register />} />
           <Route path="/documentation" element={<Documentation />} />
+          */}
           
           {/* Place new routes over this */}
           <Route path="/app/*" element={<Layout />} />
+          <Route path="*" element={<Navigate to="/app/welcome" replace />}/>
 
+          {/*
+          // Disable login
           <Route path="*" element={<Navigate to={token ? "/app/welcome" : "/login"} replace />}/>
+          */}
 
         </Routes>
       </Router>
