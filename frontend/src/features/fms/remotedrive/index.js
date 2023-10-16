65e464772d106e4c19b6e56372341e4910f32430
DennyKuo0809 <ktying8989@gmail.com>
2023-10-16 16:33:30 +0800

    Add: loading animation

--- frontend/src/features/fms/remotedrive/index.js
+++ frontend/src/features/fms/remotedrive/index.js
@@ -23,7 +23,20 @@ const VehicleSelect = forwardRef((props, ref) => {
 })
 
 
-const VehicleSelectButton = (props) => {const dispatch = useDispatch()
+const VehicleSelectButton = (props) => {
+    const dispatch = useDispatch()
+    if(props.isLoading){
+        return (
+            <div className="inline-block float-right">
+                <button className="btn px-6 btn-sm normal-case btn-primary">
+                    <svg aria-hidden="true" role="status" className="inline w-4 h-4 mr-3 text-white animate-spin" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="#E5E7EB"/>
+                        <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentColor"/>
+                    </svg>
+                </button>
+            </div>
+        )
+    }
     return(
         <button 
             className="btn px-6 btn-sm normal-case btn-info" 
@@ -44,8 +57,10 @@ function TeleopPnael() {
     const gearRef = useRef(null);
     const velocityRef = useRef(null);
     const {list} = useSelector(state => state.list)
+    const refreshLoadinig = useSelector(state => state.list.isLoading)
     const teleopScope = useSelector(state => state.teleop.teleopScope)
     const cameraUrl = useSelector(state => state.teleop.cameraUrl)
+    const teleopLoading = useSelector(state => state.teleop.isLoading)
     const [teleopStatus, setTeleopStatus] = useState( () => {
         return {
             velocity: '---',
@@ -96,7 +111,7 @@ function TeleopPnael() {
 
     return (
         <>
-            <TitleCard title={(teleopScope !== "None")?`Remote Driving on ${teleopScope}` : "Remote Driving"} TopSideButtons={<Refresh />}>
+            <TitleCard title={(teleopScope !== "None")?`Remote Driving on ${teleopScope}` : "Remote Driving"} TopSideButtons={<Refresh isLoading={refreshLoadinig}/>}>
                 <div className="grid grid-rows-4 grid-flow-col gap-4">
                     <CamImageWithStatus classname="row-span-4 col-span-10 bg-no-repeat bg-cover" bgUrl={cameraUrl} status={teleopStatus} />
 
@@ -105,7 +120,7 @@ function TeleopPnael() {
                         <div className="flex">
                             <VehicleSelect state={list} ref={scopeRef} /> 
                             <div className="inline-block w-1/4 p-2">
-                                <VehicleSelectButton text="Teleop" handleClick={startupTeleop} refon={scopeRef} reftype="select" />
+                                <VehicleSelectButton text="Teleop" handleClick={startupTeleop} refon={scopeRef} isLoading={teleopLoading} />
                             </div>
                         </div>
                     </div>
