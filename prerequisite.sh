702e43e21f6e30f39c99d86562352f9c16576f0a
ChenYing Kuo <evshary@gmail.com>
2024-02-02 10:25:52 +0800

    Add download_map into prerequisite.sh
    Signed-off-by: ChenYing Kuo <evshary@gmail.com>

--- prerequisite.sh
+++ prerequisite.sh
@@ -4,7 +4,6 @@
 curl -sSL https://install.python-poetry.org | python3 -
 poetry config virtualenvs.in-project true
 
-
 # Install packages
 sudo apt install parallel
 # Install Python packages
@@ -15,5 +14,10 @@ curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.5/install.sh | bash
 source "$HOME/.nvm/nvm.sh"
 nvm install node
 # Install npm
-cd frontend && npm install
+pushd frontend
+npm install
+popd # frontend
 
+# Install necessary data
+## map
+./download_map.sh
