fd6421e08febe58a36672c1947488f8b6b2fc30f
Leann Hsu <leann9001@gmail.com>
2024-12-23 17:17:16 +0800

    Add threading for processing images

--- zenoh_app/camera_autoware.py
+++ zenoh_app/camera_autoware.py
@@ -1,3 +1,6 @@
+import queue
+import threading
+
 import cv2
 import zenoh
 from cv_bridge import CvBridge
@@ -24,18 +27,16 @@ class MJPEG_server:
         self.server = None
         self.use_bridge_ros2dds = use_bridge_ros2dds
         self.prefix = scope if use_bridge_ros2dds else scope + '/rt'
+        
+        # Queue for threading
+        self.frame_queue = queue.Queue()
+        
+        # Start processing thread
+        self.frame_thread = threading.Thread(target=self.process_frame, daemon=True)
+        self.frame_thread.start()
 
         def callback(sample):
-            if self.clock is None:
-                return
-            
-            data = Image.deserialize(sample.payload.to_bytes())
-            camera_image_time = data.header.stamp.sec + data.header.stamp.nanosec * 1e-9
-            if self.clock - camera_image_time > MAX_FRAME_DELAY:
-                print(f"Frame dropped due to delay: {self.clock  - camera_image_time}s")
-                return
-            
-            self.camera_image = self.bridge.imgmsg_to_cv2(data, desired_encoding='passthrough')
+            self.frame_queue.put(sample)
 
         def callback_clock(sample):
             clock_data = Clock.deserialize(sample.payload.to_bytes())
@@ -54,31 +55,43 @@ class MJPEG_server:
 
     def change_scope(self, new_scope):
         def callback(sample):
-            if self.clock is None:
-                return
-            
-            data = Image.deserialize(sample.payload.to_bytes())
-            camera_image_time = data.header.stamp.sec + data.header.stamp.nanosec * 1e-9
-            if self.clock - camera_image_time > MAX_FRAME_DELAY:
-                print(f"Frame dropped due to delay: {self.clock  - camera_image_time}s")
-                return
-            
-            self.camera_image = self.bridge.imgmsg_to_cv2(data, desired_encoding='passthrough')
+            self.frame_queue.put(sample)
 
         def callback_clock(sample):
             clock_data = Clock.deserialize(sample.payload.to_bytes())
             self.clock = clock_data.clock.sec + clock_data.clock.nanosec * 1e-9
 
+        self.frame_queue.queue.clear()
         self.sub_video.undeclare()
         self.sub_clock.undeclare()
         self.prefix = new_scope if self.use_bridge_ros2dds else new_scope + '/rt'
         self.sub_video = self.session.declare_subscriber(self.prefix + IMAGE_RAW_KEY_EXPR, callback)
         self.sub_clock = self.session.declare_subscriber(self.prefix + CLOCK_KEY_EXPR, callback_clock)
         self.scope = new_scope
+        
+    def process_frame(self):
+        while True:
+            sample = self.frame_queue.get()
+            try:
+                if sample is None or self.clock is None:
+                    continue
+                
+                data = Image.deserialize(sample.payload.to_bytes())
+                camera_image_time = data.header.stamp.sec + data.header.stamp.nanosec * 1e-9
+                
+                if self.clock - camera_image_time > MAX_FRAME_DELAY:
+                    continue
+                
+                self.camera_image = self.bridge.imgmsg_to_cv2(data, desired_encoding='passthrough')
+            except Exception as e:
+                print(f"Error processing frame: {e}")
 
     def shutdown(self):
         self.sub_video.undeclare()
         self.sub_clock.undeclare()
+        self.frame_queue.put(None)
+        self.frame_thread.join()
+        
         if self.host is not None and self.port is not None:
             self.server.shutdown()
             self.server = None
@@ -111,14 +124,3 @@ class MJPEG_server:
 if __name__ == '__main__':
     s = zenoh.open()
     server = MJPEG_server(s, 'v1')
-    # import threading
-    # t = threading.Thread(target=server.run)
-    # t.start()
-    # while True:
-    #     c = input()
-    #     if c == "s":
-    #         server.shutdown()
-    #         t.join()
-    #     if c == "r":
-    #         t = threading.Thread(target=server.run)
-    #         t.start()
