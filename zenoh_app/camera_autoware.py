51e130eababdcb116cff3a3da090ab41fa14b790
Leann Hsu <leann9001@gmail.com>
2024-12-23 15:03:01 +0800

    Drop frame if delay

--- zenoh_app/camera_autoware.py
+++ zenoh_app/camera_autoware.py
@@ -3,16 +3,20 @@ import zenoh
 from cv_bridge import CvBridge
 from flask import Flask, Response
 from werkzeug.serving import make_server
-from zenoh_ros_type.common_interfaces.sensor_msgs import Image
+from zenoh_ros_type.common_interfaces import Image
+from zenoh_ros_type.rcl_interfaces import Clock
 
 IMAGE_RAW_KEY_EXPR = '/sensing/camera/traffic_light/image_raw'
+CLOCK_KEY_EXPR = '/clock'
 
+MAX_FRAME_DELAY = 0.5
 
 class MJPEG_server:
     def __init__(self, zenoh_session, scope, use_bridge_ros2dds=True):
         self.app = Flask(__name__)
         self.bridge = CvBridge()
         self.camera_image = None
+        self.clock = None
         self.session = zenoh_session
         self.scope = scope
         self.host = None
@@ -22,10 +26,23 @@ class MJPEG_server:
         self.prefix = scope if use_bridge_ros2dds else scope + '/rt'
 
         def callback(sample):
+            if self.clock is None:
+                return
+            
             data = Image.deserialize(sample.payload.to_bytes())
+            camera_image_time = data.header.stamp.sec + data.header.stamp.nanosec * 1e-9
+            if self.clock - camera_image_time > MAX_FRAME_DELAY:
+                print(f"Frame dropped due to delay: {self.clock  - camera_image_time}s")
+                return
+            
             self.camera_image = self.bridge.imgmsg_to_cv2(data, desired_encoding='passthrough')
 
-        self.sub = self.session.declare_subscriber(self.prefix + IMAGE_RAW_KEY_EXPR, callback)
+        def callback_clock(sample):
+            clock_data = Clock.deserialize(sample.payload.to_bytes())
+            self.clock = clock_data.clock.sec + clock_data.clock.nanosec * 1e-9
+
+        self.sub_video = self.session.declare_subscriber(self.prefix + IMAGE_RAW_KEY_EXPR, callback)
+        self.sub_clock = self.session.declare_subscriber(self.prefix + CLOCK_KEY_EXPR, callback_clock)
 
         @self.app.route('/')
         def index():
@@ -37,16 +54,31 @@ class MJPEG_server:
 
     def change_scope(self, new_scope):
         def callback(sample):
+            if self.clock is None:
+                return
+            
             data = Image.deserialize(sample.payload.to_bytes())
+            camera_image_time = data.header.stamp.sec + data.header.stamp.nanosec * 1e-9
+            if self.clock - camera_image_time > MAX_FRAME_DELAY:
+                print(f"Frame dropped due to delay: {self.clock  - camera_image_time}s")
+                return
+            
             self.camera_image = self.bridge.imgmsg_to_cv2(data, desired_encoding='passthrough')
 
-        self.sub.undeclare()
+        def callback_clock(sample):
+            clock_data = Clock.deserialize(sample.payload.to_bytes())
+            self.clock = clock_data.clock.sec + clock_data.clock.nanosec * 1e-9
+
+        self.sub_video.undeclare()
+        self.sub_clock.undeclare()
         self.prefix = new_scope if self.use_bridge_ros2dds else new_scope + '/rt'
-        self.sub = self.session.declare_subscriber(self.prefix + IMAGE_RAW_KEY_EXPR, callback)
+        self.sub_video = self.session.declare_subscriber(self.prefix + IMAGE_RAW_KEY_EXPR, callback)
+        self.sub_clock = self.session.declare_subscriber(self.prefix + CLOCK_KEY_EXPR, callback_clock)
         self.scope = new_scope
 
     def shutdown(self):
-        self.sub.undeclare()
+        self.sub_video.undeclare()
+        self.sub_clock.undeclare()
         if self.host is not None and self.port is not None:
             self.server.shutdown()
             self.server = None
