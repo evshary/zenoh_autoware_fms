8d5dda9d3228f7e3c7621bfe70439269d36172bf
Leann Hsu <leann9001@gmail.com>
2024-12-31 16:11:42 +0800

    Ensure proper thread synchronization

--- zenoh_app/camera_autoware.py
+++ zenoh_app/camera_autoware.py
@@ -2,10 +2,9 @@ import threading
 
 import numpy as np
 import zenoh
-from zenoh_ros_type.common_interfaces import CameraInfo
+from zenoh_ros_type.common_interfaces import Image
 
 IMAGE_RAW_KEY_EXPR = '/sensing/camera/traffic_light/image_raw'
-CAMERA_INFO_KEY_EXPR = '/sensing/camera/traffic_light/camera_info'
 
 # At 20 FPS, 10 frames represent 0.5 second of video data
 RING_CHANNEL_SIZE = 10
@@ -20,8 +19,8 @@ class MJPEG_server:
         self.prefix = scope if use_bridge_ros2dds else scope + '/rt'
         self.height = None
         self.width = None
+        self.processing = True
 
-        self.sub_info = self.session.declare_subscriber(self.prefix + CAMERA_INFO_KEY_EXPR)
         self.sub_video = self.session.declare_subscriber(self.prefix + IMAGE_RAW_KEY_EXPR, zenoh.handlers.RingChannel(RING_CHANNEL_SIZE))
 
         # Start processing thread
@@ -29,35 +28,43 @@ class MJPEG_server:
         self.frame_thread.start()
 
     def change_scope(self, new_scope):
-        self.sub_info.undeclare()
+        self.processing = False
+        self.frame_thread.join()
+
         self.sub_video.undeclare()
 
         self.prefix = new_scope if self.use_bridge_ros2dds else new_scope + '/rt'
-        self.sub_info = self.session.declare_subscriber(self.prefix + CAMERA_INFO_KEY_EXPR)
         self.sub_video = self.session.declare_subscriber(self.prefix + IMAGE_RAW_KEY_EXPR, zenoh.handlers.RingChannel(RING_CHANNEL_SIZE))
         self.scope = new_scope
 
         self.width = None
         self.height = None
+        
+        self.processing = True
+        self.frame_thread = threading.Thread(target=self.process_frame, daemon=True)
+        self.frame_thread.start()
 
     def process_frame(self):
-        while True:
+        while self.processing:
             try:
                 if self.width is None or self.height is None:
                     sample = self.sub_video.try_recv()
                     if sample is None:
                         continue
-                    camera_info = CameraInfo.deserialize(sample.payload.to_bytes())
-                    self.height = camera_info.height
-                    self.width = camera_info.width
+                    image = Image.deserialize(sample.payload.to_bytes())
+                    self.height = image.height
+                    self.width = image.width
 
                 sample = self.sub_video.try_recv()
                 if sample is None:
                     continue
 
                 data = sample.payload.to_bytes()
-                np_data = np.frombuffer(data[-(self.height * self.width * 4) :], dtype=np.uint8)
-                self.camera_image = np_data.reshape((self.height, self.width, 4))
+                
+                # Each pixel is 4 bytes (RGBA), total bytes = Height x Width x 4.
+                # Extract the last part of the ROS message as image data.
+                np_image = np.frombuffer(data[-(self.height * self.width * 4) :], dtype=np.uint8)
+                self.camera_image = np_image.reshape((self.height, self.width, 4))
 
             except Exception as e:
                 print(f'Error processing frame: {e}')
