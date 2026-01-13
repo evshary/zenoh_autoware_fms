2abc0cf2eb88d8dc699888ffc2bdee8463ccae19
ChenYing Kuo <evshary@gmail.com>
2026-01-13 14:04:12 +0800

    Migrate from poetry to uv
    Signed-off-by: ChenYing Kuo <evshary@gmail.com>

--- prerequisite.sh
+++ prerequisite.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 
-# Install poetry
-curl -sSL https://install.python-poetry.org | python3 -
-poetry config virtualenvs.in-project true
-
 # Install packages
 sudo apt install parallel
+
+# Install uv
+curl -LsSf https://astral.sh/uv/install.sh | sh
 # Install Python packages
-poetry install
+uv sync
 
 # Install nodejs
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.5/install.sh | bash
--- run_server.sh
+++ run_server.sh
@@ -2,5 +2,5 @@
 
 echo "Connect to http://127.0.0.1:8080"
 parallel --verbose --lb ::: \
-    'poetry run uvicorn api_server:app --reload 2>&1 | tee logs/api_server.log' \
+    'uv run uvicorn api_server:app --reload 2>&1 | tee logs/api_server.log' \
     'cd frontend && npm start | tee logs/frontend.log'
